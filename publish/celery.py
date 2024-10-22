da60b1ce0b2c5cb0c34626eb8a13672a52419123
Sergey <main0serg@gmail.com>
2024-10-22 13:57:17 +0600

    added email sendler for every days

--- publish/celery.py
+++ publish/celery.py
@@ -1,6 +1,7 @@
 import os
 
 from celery import Celery
+from celery.schedules import crontab
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "django_celery.settings")
 app = Celery(
@@ -12,3 +13,9 @@ app.config_from_object("django.conf:settings")
 broker_connection_retry = False
 
 app.autodiscover_tasks()
+app.conf.beat_schedule = {
+    "send-report-every-single-minute": {
+        "task": "publish.tasks.send_view_count_report",
+        "schedule": crontab(minute=0, hour=0),
+    },
+}
--- publish/models.py
+++ publish/models.py
@@ -8,10 +8,10 @@ User = get_user_model()
 
 class Post(models.Model):
     author = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
-    created = models.DateTimeField('Created Date', default=timezone.now)
-    title = models.CharField('Title', max_length=200)
-    content = models.TextField('Content')
-    slug = models.SlugField('Slug')
+    created = models.DateTimeField("Created Date", default=timezone.now)
+    title = models.CharField("Title", max_length=200)
+    content = models.TextField("Content")
+    slug = models.SlugField("Slug")
     view_count = models.IntegerField("View Count", default=0)
 
     def get_absolute_url(self):
