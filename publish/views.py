e98574df35b5960af49bfe10ffbeee6a50e5c7a2
Sergey <main0serg@gmail.com>
2024-10-22 12:44:34 +0600

    added Post view

--- publish/views.py
+++ publish/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 
-# Create your views here.
+from .models import Post
+
+
+def view_post(request, slug):
+    post = get_object_or_404(Post, slug=slug)
+    return render(request, "post.html", context={"post": post})
