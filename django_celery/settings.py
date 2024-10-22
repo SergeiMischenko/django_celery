7d974c3a411c5a06e9dce5acf1b7e01de3c5b5e8
Sergey <main0serg@gmail.com>
2024-10-22 13:06:51 +0600

    added gmail backend

--- django_celery/settings.py
+++ django_celery/settings.py
@@ -89,3 +89,13 @@ USE_TZ = True
 STATIC_URL = "static/"
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+# EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+# Конфигурация сервера электронной почты
+EMAIL_HOST = "smtp.gmail.com"
+EMAIL_HOST_USER = str(os.getenv("EMAIL_HOST_USER"))
+EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
